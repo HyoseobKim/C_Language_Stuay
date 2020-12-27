076c542ea4a4372cdedd3306829951eb26a745e1
HyoseobKim <71328867+HyoseobKim@users.noreply.github.com>
2020-12-26 21:42:07 -0500

    Created the project

---
+++ Intro/Intro/main.c
@@ -0,0 +1,14 @@
+//
+//  main.c
+//  Intro
+//
+//  Created by Hyoseob Kim on 2020-12-26.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
